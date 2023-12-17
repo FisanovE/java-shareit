e1ccc8f609ab045bc614d2514ccc7e77fb96640b
FisanovE <fj1983@mail.ru>
2023-12-17 18:24:31 +0500

    fix: добавить кодировку в PrintStream в ErrorHandler.

--- gateway/src/main/java/ru/practicum/shareit/common/exeptions/ErrorHandler.java
+++ gateway/src/main/java/ru/practicum/shareit/common/exeptions/ErrorHandler.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.io.UnsupportedEncodingException;
 import java.nio.charset.Charset;
 
 @Slf4j
@@ -16,40 +17,40 @@ public class ErrorHandler {
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorResponse handle(final ValidationException e) {
+    public ErrorResponse handle(final ValidationException e) throws UnsupportedEncodingException {
         log.error("400 {}", e.getMessage(), e);
         ByteArrayOutputStream out = new ByteArrayOutputStream();
-        e.printStackTrace(new PrintStream(out));
+        e.printStackTrace(new PrintStream(out, true, "UTF-8"));
         String stackTrace = out.toString(Charset.forName("UTF-8"));
         return new ErrorResponse(e.getMessage(), stackTrace);
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ErrorResponse handle(final NotFoundException e) {
+    public ErrorResponse handle(final NotFoundException e) throws UnsupportedEncodingException {
         log.error("404 {}", e.getMessage(), e);
         ByteArrayOutputStream out = new ByteArrayOutputStream();
-        e.printStackTrace(new PrintStream(out));
+        e.printStackTrace(new PrintStream(out, true, "UTF-8"));
         String stackTrace = out.toString(Charset.forName("UTF-8"));
         return new ErrorResponse(e.getMessage(), stackTrace);
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.CONFLICT)
-    public ErrorResponse handle(final ConflictDataException e) {
+    public ErrorResponse handle(final ConflictDataException e) throws UnsupportedEncodingException {
         log.error("409 {}", e.getMessage(), e);
         ByteArrayOutputStream out = new ByteArrayOutputStream();
-        e.printStackTrace(new PrintStream(out));
+        e.printStackTrace(new PrintStream(out, true, "UTF-8"));
         String stackTrace = out.toString(Charset.forName("UTF-8"));
         return new ErrorResponse(e.getMessage(), stackTrace);
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public ErrorResponse handle(final UnsupportedStatusException e) {
+    public ErrorResponse handle(final UnsupportedStatusException e) throws UnsupportedEncodingException {
         log.error("500 {}", e.getMessage(), e);
         ByteArrayOutputStream out = new ByteArrayOutputStream();
-        e.printStackTrace(new PrintStream(out));
+        e.printStackTrace(new PrintStream(out, true, "UTF-8"));
         String stackTrace = out.toString(Charset.forName("UTF-8"));
         return new ErrorResponse(e.getMessage(), stackTrace);
     }
